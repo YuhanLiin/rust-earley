8ac2a46d44ef8905ae6e319edf5b39e8664a9136
YuhanLiin <linyuhan0315@hotmail.com>
2019-03-02 18:13:11 -0500

    Add tests

--- src/lib.rs
+++ src/lib.rs
@@ -36,6 +36,9 @@ macro_rules! grammar {
                 $($lhs),*
             }
 
+            #[derive(Debug)]
+            #[derive(PartialEq)]
+            #[derive(Eq)]
             pub enum Symbol{
                 Terminal($Token),
                 NonTerminal(NonTerminal),
@@ -63,9 +66,10 @@ macro_rules! grammar {
                     self.0.iter()
                 }
 
-                pub fn len(&self) -> usize {
-                    self.0.len()
-                }
+                pub fn len(&self) -> usize { self.0.len() }
+
+
+                pub fn symbol(&self, i: usize) -> &Symbol { &self.0[i] }
             }
 
             pub struct Grammar {
@@ -82,10 +86,10 @@ macro_rules! grammar {
                     prod_rules.push(Rule(rhs));
                 }
 
-                pub fn iter_rhs<T>(&self, lhs: &NonTerminal)
-                -> Option<impl Iterator<Item=&Rule>>
+                pub fn get_iter_rhs(&self, lhs: &NonTerminal)
+                -> impl Iterator<Item=&Rule>
                 {
-                    self.rules.get(lhs).map(|vec| vec.iter())
+                    self.rules.get(lhs).map(|vec| vec.iter()).unwrap()
                 }
 
                 pub fn iter_lhs(&self) -> impl Iterator<Item=&NonTerminal> {
@@ -111,9 +115,14 @@ mod tests {
     use std::collections::HashSet;
     use std::iter::FromIterator;
 
+    #[derive(Debug)]
+    #[derive(PartialEq)]
+    #[derive(Eq)]
     enum Tok {
         NUM, PLUS, MINUS,
     }
+
+    grammar!(EmptyGrammar, crate::tests::Tok,);
 
     grammar!(TestGrammar, crate::tests::Tok,
              empty = []
@@ -122,15 +131,51 @@ mod tests {
     );
 
     #[test]
-    fn it_works() {
+    fn empty() {
+        use crate::tests::EmptyGrammar::*;
+
+        let grammar = get_grammar();
+
+        assert_eq!(grammar.iter_lhs().count(), 0);
+    }
+
+    #[test]
+    fn grammar() {
         use crate::tests::TestGrammar::*;
 
         let grammar = get_grammar();
 
         let non_terminals: HashSet<&NonTerminal> =
             HashSet::from_iter(grammar.iter_lhs());
-        assert!(non_terminals.get(&NonTerminal::empty).is_some());
-        assert!(non_terminals.get(&NonTerminal::stmt).is_some());
-        assert!(non_terminals.get(&NonTerminal::expr).is_some());
+
+        let rules =
+            Vec::from_iter(grammar.get_iter_rhs(&NonTerminal::empty));
+        assert_eq!(rules.len(), 1);
+        assert_eq!(rules[0].len(), 0);
+
+        let rules =
+            Vec::from_iter(grammar.get_iter_rhs(&NonTerminal::stmt));
+        assert_eq!(rules.len(), 2);
+        assert_eq!(rules[0].len(), 1);
+        assert_eq!(rules[0].symbol(0), &Symbol::NonTerminal(NonTerminal::expr));
+        assert_eq!(rules[1].len(), 0);
+
+        let rules =
+            Vec::from_iter(grammar.get_iter_rhs(&NonTerminal::expr));
+        assert_eq!(rules.len(), 3);
+        let rules = Vec::from_iter(rules.iter().map(|rule| {
+            Vec::from_iter(rule.iter())
+        }));
+        assert_eq!(rules[0], vec![&Symbol::Terminal(Tok::NUM)]);
+        assert_eq!(rules[1], vec![
+            &Symbol::NonTerminal(NonTerminal::expr),
+            &Symbol::Terminal(Tok::PLUS),
+            &Symbol::NonTerminal(NonTerminal::expr),
+        ]);
+        assert_eq!(rules[2], vec![
+            &Symbol::NonTerminal(NonTerminal::expr),
+            &Symbol::Terminal(Tok::MINUS),
+            &Symbol::NonTerminal(NonTerminal::expr),
+        ]);
     }
 }
