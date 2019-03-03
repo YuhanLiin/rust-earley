32be3f67de7d7824935f57c241c081416ae4b289
YuhanLiin <linyuhan0315@hotmail.com>
2019-03-03 16:00:33 -0500

    Integrated 2 newtypes

--- src/lib.rs
+++ src/lib.rs
@@ -29,11 +29,54 @@ where R: Rule<'a, Symbol=S>, S: 'a
     }
 }
 
+
+struct StateSet<'a, S: 'a, R: Rule<'a, Symbol=S>, NT> (Vec<Item<'a, S, R, NT>>);
+
+impl<'a, S, R, NT> StateSet<'a, S, R, NT> 
+where R: Rule<'a, Symbol=S>, S: 'a
+{
+    fn new() -> Self { Self (Vec::new()) }
+
+    fn push(&mut self, item: Item<'a, S, R, NT>) {
+        self.0.push(item);
+    }
+
+    fn merge(&mut self, mut other: Self) {
+        self.0.append(&mut other.0);
+    }
+
+    fn iter(&self) -> impl Iterator<Item=&Item<'a, S, R, NT>> {
+        self.0.iter()
+    }
+}
+
+struct Chart<'a, S: 'a, R: Rule<'a, Symbol=S>, NT> (Vec<StateSet<'a, S, R, NT>>);
+
+impl<'a, S, R, NT> Chart<'a, S, R, NT> 
+where R: Rule<'a, Symbol=S>, S: 'a
+{
+    fn new() -> Self { Self(Vec::new()) }
+
+    fn len(&self) -> usize { self.0.len() }
+
+    fn push(&mut self, set: StateSet<'a, S, R, NT>) { 
+        self.0.push(set);
+    }
+
+    fn get(&self, i: usize) -> &StateSet<'a, S, R, NT> {
+        &self.0[i]
+    }
+
+    fn get_mut(&mut self, i: usize) -> &mut StateSet<'a, S, R, NT> {
+        &mut self.0[i]
+    }
+}
+
 struct Parser<'a, G, S: 'a, R, NT: 'a>
 where R: Rule<'a, Symbol=S>, G: Grammar<'a, NonTerminal=NT, Rule=R>
 {
     grammar: &'a G,
-    chart: Vec<Vec<Item<'a, S, R, NT>>>,
+    chart: Chart<'a, S, R, NT>,
     progress: usize,
 }
 
@@ -44,41 +87,28 @@ where R: Rule<'a, Symbol=S>,
       NT: Eq
 {
     fn new(grammar: &'a G) -> Self {
-        Self { chart: Vec::new(), grammar, progress: 0 }
-    }
-
-    fn get_set(&self, i: usize) -> &Vec<Item<'a, S, R, NT>> {
-        &self.chart[i]    
-    }
-
-    fn get_set_or_add(&mut self, i: usize) -> &mut Vec<Item<'a, S, R, NT>> {
-        assert!(self.chart.len() - i <= 2);
-        if i >= self.chart.len() {
-            self.chart.push(Vec::new());
-        }
-        // Given the above assert, this should not panic
-        &mut self.chart[i]
+        Self { chart: Chart::new(), grammar, progress: 0 }
     }
 
     fn predict(&mut self, non_term: &'a NT) {
         // TODO repeat prevention
         let progress = self.progress;
         for rule in self.grammar.get_iter_rhs(non_term) {
-            self.get_set_or_add(progress).
+            self.chart.get_mut(progress).
                  push(Item::new(non_term, rule, progress));
         }
     }
 
     fn scan(&mut self, item: &Item<'a, S, R, NT>) {
         // TODO token checking
-        self.get_set_or_add(self.progress + 1).push(item.advance());
+        self.chart.get_mut(self.progress + 1).push(item.advance());
     }
 
     fn complete(&mut self, item: &Item<'a, S, R, NT>) {
         assert!(item.done());
         let mut temp = Vec::new();
         let from = item.from;
-        for old_item in self.get_set(from) {
+        for old_item in self.chart.get(from).iter() {
             if let Some(sym) = old_item.dot_symbol() {
                 if let Some(nt) = sym.nonterminal() {
                     if nt == item.lhs {
@@ -88,14 +118,18 @@ where R: Rule<'a, Symbol=S>,
             }
         }
         for item in temp {
-            self.get_set_or_add(self.progress + 1).push(item);
+            self.chart.get_mut(self.progress + 1).push(item);
         }
     }
 
     fn parse_token(&mut self, token: T) {
-        for item in self.get_set(self.progress) {
+        for item in self.chart.get(self.progress).iter() {
+            if let Some(symbol) = item.dot_symbol() {
+                
+            } else {
+                //self.complete(item);
+            }
         }
         self.progress += 1;
-
     }
 }
