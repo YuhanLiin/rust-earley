2f9fdcf0b7d5b0be500b06ea0b11df51609d1522
YuhanLiin <linyuhan0315@hotmail.com>
2019-03-16 18:50:05 -0400

    Add tests for cyclical grammars

--- src/grammar.rs
+++ src/grammar.rs
@@ -405,3 +405,44 @@ mod tests {
         assert!(grammar.is_nullable(NonTerminal::I));
     }
 }
+
+#[cfg(test)]
+mod panic_tests {
+    #[derive(Debug, PartialEq, Eq, Clone, Copy)]
+    pub enum Tok {
+        PLUS,
+        MINUS,
+    }
+
+    grammar!(null_cycle_grammar <crate::grammar::panic_tests::Tok>:
+             S = [S S | S S S | ]);
+
+    #[test]
+    #[should_panic(expected = "infinite ambiguity")]
+    fn nullable_cycle() {
+        null_cycle_grammar::get_grammar();
+    }
+
+    grammar!(null_cycle_grammar2 <crate::grammar::panic_tests::Tok>:
+             S = [ A B ]
+             A = [ B ]
+             B = [ S | ]);
+
+    #[test]
+    #[should_panic(expected = "infinite ambiguity")]
+    fn long_nullable_cycle() {
+        null_cycle_grammar2::get_grammar();
+    }
+
+    grammar!(cycle_grammar <crate::grammar::panic_tests::Tok>:
+             Good = [ PLUS | MINUS ]
+             Bad = [ Worse ]
+             Worse = [ Worst ]
+             Worst = [ Good | Bad ]);
+
+    #[test]
+    #[should_panic(expected = "infinite ambiguity")]
+    fn non_nullable_cycle() {
+        cycle_grammar::get_grammar();
+    }
+}
